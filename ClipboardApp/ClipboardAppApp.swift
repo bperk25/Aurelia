0690d484e0d9c079b2bfdfeffae019da1e2ae94b
bperk25 <91712630+bperk25@users.noreply.github.com>
2025-01-01 18:16:33 -0600

    Initial Commit

---
+++ ClipboardApp/ClipboardAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipboardAppApp.swift
+//  ClipboardApp
+//
+//  Created by Bryant Perkins on 1/1/25.
+//
+
+import SwiftUI
+
+@main
+struct ClipboardAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClipboardAppTests/ClipboardAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClipboardAppTests.swift
+//  ClipboardAppTests
+//
+//  Created by Bryant Perkins on 1/1/25.
+//
+
+import Testing
+@testable import ClipboardApp
+
+struct ClipboardAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
